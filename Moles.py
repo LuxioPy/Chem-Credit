cd10a1191de21dc778760241212ad5b1c02b3318
Zion Ceus <zionceus@gmail.com>
2024-03-03 14:26:18 -0500

    color change

--- Moles.py
+++ Moles.py
@@ -1,5 +1,6 @@
 import streamlit as st 
 import pandas as pd
+import math 
 
 periodic_data = pd.read_csv("https://gist.githubusercontent.com/GoodmanSciences/c2dd862cd38f21b0ad36b8f96b4bf1ee/raw/1d92663004489a5b6926e944c1b3d9ec5c40900e/Periodic%2520Table%2520of%2520Elements.csv")
 def main():
@@ -8,7 +9,13 @@ def main():
     if user_element_dropdown:
         mass = st.number_input("Enter the mass of the element (in grams): ")
         if st.button("Calculate Number of Moles"):
-            element_data = periodic_data.loc[periodic_table_data["elements"].apply(lambda x: x["name"]) == selected_element].iloc[0]["elements"]
+            atomic_mass = periodic_data.loc[periodic_data["Element"] == user_element_dropdown, "AtomicMass"].iloc[0]
+            numofMoles = mass/atomic_mass
+            st.write(f"There is {numofMoles:.2f}*10^{int(math.log10(numofMoles))} moles of {str(user_element_dropdown)} in {mass:.2f}g")
+
+        
+
+
 
 
 if __name__ == "__main__":
